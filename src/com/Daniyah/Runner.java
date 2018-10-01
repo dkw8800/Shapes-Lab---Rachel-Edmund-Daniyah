286c06d1ebceed6999dc95a652496470c56199b7
Edmund Wu <fangkazuto@gmail.com>
2018-10-01 17:00:17 -0400

    Include class ETriangle

--- src/com/Daniyah/Runner.java
+++ src/com/Daniyah/Runner.java
@@ -3,7 +3,7 @@ package com.Daniyah;
 public class Runner {
     public static void main (String[] args) {
         Shape shape1 = new Rectangle(10, 5);
-        //Shape shape2 = new ETriangle(5);
+        Shape shape2 = new ETriangle(5);
         Shape shape3 = new Circle(10);
 
         Shape shape4 = new RPentagon(5);
@@ -11,8 +11,7 @@ public class Runner {
         System.out.println("My Perimeter is: " + shape4.getPerimeter());
         System.out.println("My Area is: " + shape4.getArea());
 
-        //Shape[] shapes = { shape1, shape2, shape3 };
-        Shape[] shapes = { shape1, shape3 };
+        Shape[] shapes = { shape1, shape2, shape3 };
 
         for (Shape cur : shapes) {
             System.out.println(cur);
